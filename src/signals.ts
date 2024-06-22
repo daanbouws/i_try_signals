976f08b6e6c76370b36df855b3f7b59b363a7e1f
Daan Bouws <dbouws@cloudsuite.com>
2024-06-22 12:57:54 +0200

    found out how they make it work without the extra parameter to effect

--- src/signals.ts
+++ src/signals.ts
@@ -2,21 +2,36 @@ type Signal<T> = {
   get: () => T,
   set: (value: T) => void,
   peek: () => T,
-  subscribe: (fn: any) => void
+}
+
+type EffectWrapper = {
+  execute: () => void,
+  dependencies: Set<any>,
+}
+
+const manager: EffectWrapper[] = []
+
+const subscribe = (effect: EffectWrapper, subscriptions: Set<EffectWrapper>) => {
+  subscriptions.add(effect)
+  effect.dependencies.add(subscriptions)
 }
 
 export const signal = <T>(defaultValue: T): Signal<T> => {
   let _ = defaultValue
-  const subscribers: Array<Function> = []
+  const subscribers: Set<EffectWrapper> = new Set()
 
   // call all the effects and computes
   const notify = () => {
-    for (let i =0; i < subscribers.length; i++) {
-      subscribers[i]()
+    for (const sub of [...subscribers]) {
+      sub.execute();
     }
   }
 
   const get = (): T => {
+    const effect = manager.at(-1)
+    if (effect) {
+      subscribe(effect, subscribers)
+    }
     return _
   }
 
@@ -29,35 +44,43 @@ export const signal = <T>(defaultValue: T): Signal<T> => {
     return _
   }
 
-  const subscribe = (fn: any): void => {
-    subscribers.push(fn)
-  }
-
   return {
     get,
     set,
     peek,
-    subscribe,
   }
 }
 
-export const effect = (fn: any, signals: Array<Signal<any>>) => {
-   signals.forEach(signal => signal.subscribe(fn))
-  try {
-    fn()
-  } catch(e) {
-    console.log(e)
+type EffectFn = (...args: any[]) => void
+
+
+
+export const effect = (fn: EffectFn) => {
+  const execute = () => {
+    manager.push(stuff)
+    try {
+      fn()
+    } catch(e) {
+      console.log(e)
+    }
   }
+
+  const stuff: EffectWrapper = {
+    execute,
+    dependencies: new Set()
+  }
+
+  execute()
 }
 
-export const computed = (fn: any, signals: Array<Signal<any>>) => {
+// export const computed = (fn: any, signals: Array<Signal<any>>) => {
 
-}
+// }
 
 const testSignal = signal(100)
 
 effect(() => {
   console.log(testSignal.get())
-}, [testSignal])
+})
 
 testSignal.set(101)
